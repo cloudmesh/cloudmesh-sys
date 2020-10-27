6cea057e0173ec4ace574e57d8b43b7376bbab96
Gregor von Laszewski <laszewski@gmail.com>
2020-10-27 10:19:41 -0400

    add unit test

---
+++ tests/test_00_sys.py
@@ -0,0 +1,38 @@
+###############################################################
+# pytest -v --capture=no tests/test_00_sys.py
+# pytest -v  tests/test_00_sys.py
+###############################################################
+
+import pytest
+from cloudmesh.common.Benchmark import Benchmark
+from cloudmesh.common.Shell import Shell
+from cloudmesh.common.debug import VERBOSE
+from cloudmesh.common.util import HEADING
+from cloudmesh.common.variables import Variables
+
+Benchmark.debug()
+
+variables = Variables()
+
+cloud = variables.parameter('cloud')
+
+print(f"Test run for {cloud}")
+
+if cloud is None:
+    raise ValueError("cloud is not set")
+
+
+@pytest.mark.incremental
+class Test_Sys:
+
+    def test_cms_help(self):
+        HEADING()
+
+        Benchmark.Start()
+        result = Shell.execute("cms help", shell=True)
+        Benchmark.Stop()
+        VERBOSE(result)
+        assert 'sys' in result
+
+    def test_benchmark(self):
+        Benchmark.print(sysinfo=True, csv=True, tag=cloud)
