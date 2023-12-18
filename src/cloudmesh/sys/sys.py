abe451b60a6be13c0751888be3d1ad6c7867e2d7
Gregor von Laszewski <laszewski@gmail.com>
2023-12-18 13:00:35 -0500

    * add docstrings

--- src/cloudmesh/sys/sys.py
+++ src/cloudmesh/sys/sys.py
@@ -1,5 +1,13 @@
-"""
-Managing the cmd5 system installation and package distribution
+"""Module for managing the cmd5 system installation and package distribution.
+
+This module provides classes to generate cmd5 command templates, perform Git management,
+and set the version number of base packages.
+
+Classes:
+    - Sys: Class to generate cmd5 command templates.
+    - Git: Git management for the preparation to upload the code to PyPI.
+    - Version: Set the version number of all base packages.
+
 """
 import os
 import textwrap
@@ -11,19 +19,20 @@ from cloudmesh.common.util import banner
 from cloudmesh.common.util import readfile
 from cloudmesh.common.util import writefile
 
-from cloudmesh.sys.__version__ import version
 
 class Sys:
-    """
-    Class to generate cmd5 command templates
-    """
+    """Class to generate cmd5 command templates."""
 
     @classmethod
     def generate(cls, name):
-        """
-        Generate a command template with the given name
-        :param name: the name of the command
-        :return:
+        """Generate a command template with the given name.
+
+        Args:
+            name (str): The name of the command.
+
+        Returns:
+            str: Empty string if generation fails, otherwise, a message indicating success.
+
         """
 
         if name.startswith("cloudmesh-"):
@@ -136,9 +145,7 @@ class Sys:
 
 
 class Git:
-    """
-    Git management for the preparation to upload the code to pypi
-    """
+    """Git management for the preparation to upload the code to PyPI."""
 
     pypis = ["cloudmesh-common", "cloudmesh-cmd5", "cloudmesh-sys"]
     commits = pypis + ["cloudmesh-bar"]
@@ -148,9 +155,11 @@ class Git:
 
     @classmethod
     def upload(cls):
-        """
-        upload the code to pypi
-        :return:
+        """Upload the code to PyPI.
+
+        Returns:
+            None
+
         """
 
         banner("CREATE DIST")
@@ -171,12 +180,15 @@ class Git:
 
     @classmethod
     def commit(cls, msg):
-        """
-        commit the current code to git
-        :param msg:
-        :return:
-        """
+        """Commit the current code to Git.
 
+        Args:
+            msg (str): Commit message.
+
+        Returns:
+            None
+
+        """
         banner("COMMIT " + msg)
         for p in cls.commits:
             banner("repo " + p)
@@ -185,16 +197,18 @@ class Git:
 
 
 class Version:
-    """
-    set the version number of all base packages
-    """
+    """Set the version number of all base packages."""
 
     @classmethod
     def set(cls, version):
-        """
-        set the version number
-        :param version: the version as text string
-        :return:
+        """Set the version number.
+
+        Args:
+            version (str): The version as a text string.
+
+        Returns:
+            None
+
         """
         for repo in Git.commits:
             print(repo, "->", version)
